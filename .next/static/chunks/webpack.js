b39df33ee359a8eda24d7699b5a4e487bb490257
Eliomatheus <eliomatheus8@gmail.com>
2024-08-08 22:26:57 -0300

    chore: update shadcs theme

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5d07fce62e19b2f"; }
+/******/ 		__webpack_require__.h = function() { return "63d429b798e2fe71"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
