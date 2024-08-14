8a669f8b6ca6209b242a8d6518d7cb761acd7916
Elio Matheus <eliomatheus8@gmail.com>
2024-08-13 21:46:24 -0300

    feat: add BookngItem component

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
-import { Badge } from "./_components/ui/badge"
-import { Avatar, AvatarImage } from "./_components/ui/avatar"
 import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
+import { quickSearchOption } from "./_constants/search"
+import { BookingItem } from "./_components/booking-item"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
@@ -36,65 +36,17 @@ const Home = async () => {
 
         {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
-          <Button className="gap-2" variant="secondary">
-            <Image
-              alt="Busca Rapida de Cabelo"
-              src="/cabelo.svg"
-              width={16}
-              height={16}
-            />
-            Cabelo
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              alt="Busca Rapida de barba"
-              src="/barba.svg"
-              width={16}
-              height={16}
-            />
-            Barba
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              alt="Busca Rapida de acabamento"
-              src="/acabamento.svg"
-              width={16}
-              height={16}
-            />
-            Acabamento
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              alt="Busca Rapida de acabamento"
-              src="/sombrancelha.svg"
-              width={16}
-              height={16}
-            />
-            Sobrancelha
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              alt="Busca Rapida de acabamento"
-              src="/massagem.svg"
-              width={16}
-              height={16}
-            />
-            Massagem
-          </Button>
-
-          <Button className="gap-2" variant="secondary">
-            <Image
-              alt="Busca Rapida de acabamento"
-              src="/hidratacao.svg"
-              width={16}
-              height={16}
-            />
-            Hidratação
-          </Button>
+          {quickSearchOption.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
         </div>
 
         {/* IMAGEM */}
@@ -108,28 +60,8 @@ const Home = async () => {
         </div>
 
         {/* AGENDAMENTO*/}
-        <Card className="mt-6">
-          <CardContent className="flex justify-between p-0">
-            {/* ESQUERDA*/}
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">Confirmado</Badge>
-              <h3 className="font-semibold">Corte de Cabelo</h3>
+        <BookingItem />
 
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sm">Barbearia FSW</p>
-              </div>
-            </div>
-            {/* DIREITA */}
-            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
-              <p className="text-sm">Agosto</p>
-              <p className="text-2xl">12</p>
-              <p className="text-sm">20:00</p>
-            </div>
-          </CardContent>
-        </Card>
         {/* RECOMENDADOS */}
         <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
           Recomendados
